7ad6141113319d6f2d90df76e9ae0cebd40ebead
ejariasa <yuxi-eylen@hotmail.com>
2018-02-08 14:09:10 -0500

    exercise chuck norris

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,24 @@
+import { HttpModule } from '@angular/http';
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: [ './app.component.css' ]
 })
 export class AppComponent {
-  constructor() {}
-  ngOnInit(){}
+	prueba: string;
+	url = 'https://api.icndb.com/jokes/random';
+	objeto: any;
+	constructor(private client: HttpClient) {}
+	ngOnInit() {
+		setInterval(() => {
+			this.getData();
+		}, 5000);
+	}
 
+	getData(): void {
+		this.client.get(this.url).subscribe((data) => (this.prueba = data['value']['joke']));
+	}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
-
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    HttpModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [ AppComponent ],
+	imports: [ BrowserModule, HttpModule, HttpClientModule ],
+	providers: [],
+	bootstrap: [ AppComponent ]
 })
-export class AppModule { }
+export class AppModule {}
